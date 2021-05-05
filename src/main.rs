d8d7609de24fac1b616aa0fa0fca6ffbab9dd810
Suilied <Suilied@hotmail.nl>
2021-05-05 22:22:35 +0200

    startings of casting

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy::render::pass::ClearColor;
 use bevy::app::AppExit;
 
 const MAX_MOVE_SPEED: f32 = 2.;
+const MOTE_SPAWN_SPEED: f32 = 0.5;
 
 struct Player {
     bounce_offset: f32,
@@ -10,6 +11,13 @@ struct Player {
     bounce_power: f32,
     posx: f32,
     posy: f32,
+
+    casting: bool,
+
+    anger: f32,
+    joy: f32,
+    fear: f32,
+    sadness: f32,
 }
 
 fn player_movement(
@@ -42,6 +50,37 @@ fn player_movement(
         if keyboard_input.pressed(KeyCode::Escape) {
             println!("!!!ESCAPE!!!");
             exit.send(AppExit);
+        }
+
+        if keyboard_input.just_pressed(KeyCode::Space){
+            player.casting = !player.casting;
+            let cast_string = if player.casting {"Player is casting!"} else { "player stopped casting.." };
+            println!("{}", cast_string);
+            println!("Motes: \nAnger: {} | Fear   : {}\nJoy  : {} | Sadness: {}", player.anger, player.fear, player.joy, player.sadness);
+        }
+
+        if keyboard_input.pressed(KeyCode::W) {
+            if player.casting {
+                player.anger += MOTE_SPAWN_SPEED;
+            }
+        }
+
+        if keyboard_input.pressed(KeyCode::S) {
+            if player.casting {
+                player.fear += MOTE_SPAWN_SPEED;
+            }
+        }
+
+        if keyboard_input.pressed(KeyCode::A) {
+            if player.casting {
+                player.joy += MOTE_SPAWN_SPEED;
+            }
+        }
+
+        if keyboard_input.pressed(KeyCode::D) {
+            if player.casting {
+                player.sadness += MOTE_SPAWN_SPEED;
+            }
         }
 
         let start_bounce = keyboard_input.just_pressed(KeyCode::Left) || keyboard_input.just_pressed(KeyCode::Right) || keyboard_input.just_pressed(KeyCode::Up) || keyboard_input.just_pressed(KeyCode::Down);
@@ -96,5 +135,12 @@ fn setup(
         bounce_power: 2.,
         posx: 0.,
         posy: 0.,
+
+        casting: false,
+
+        anger: 0.,
+        joy: 0.,
+        fear: 0.,
+        sadness: 0.,
     });
 }
