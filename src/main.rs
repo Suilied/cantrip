694b8a5cdc6c8cfc935a18d9eb2690798b597821
Suilied <Suilied@hotmail.nl>
2021-05-07 08:03:00 +0200

    Added v1 of manabars

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use bevy::prelude::*;
-use bevy::render::pass::ClearColor;
 use bevy::app::AppExit;
 
 const MAX_MOVE_SPEED: f32 = 2.;
 const MOTE_SPAWN_SPEED: f32 = 0.5;
+
+enum Affinity {
+    Anger,
+    Joy,
+    Fear,
+    Sadness,
+}
+
+struct ManaBar {
+    size: f32,
+    pos: Vec2,
+    affinity: Affinity,
+}
 
 struct Player {
     bounce_offset: f32,
@@ -20,12 +32,26 @@ struct Player {
     sadness: f32,
 }
 
+fn update_manabars(
+    mut manabars: Query<(&mut Transform, &mut Sprite, &mut ManaBar)>,
+) {
+    // change this so that it's drawn on the UI layer
+    for (mut transform, mut manasprite, bar) in manabars.iter_mut() {
+        manasprite.size.y = bar.size;
+        transform.translation.x = bar.pos.x; 
+        transform.translation.y = bar.pos.y;
+    }
+
+}
+
 fn player_movement(
     keyboard_input: Res<Input<KeyCode>>,
     mut position_players: Query<(&mut Transform, &mut Sprite, &mut Player)>,
+    mut manabars: Query<&mut ManaBar>,
     mut exit: EventWriter<AppExit>,
 ) {
     for (mut transform, mut sprite, mut player) in position_players.iter_mut() {
+
         let mut stop_bounce = true;
         if keyboard_input.pressed(KeyCode::Left) {
             player.posx -= MAX_MOVE_SPEED;
@@ -83,6 +109,22 @@ fn player_movement(
             }
         }
 
+        for mut mbars in manabars.iter_mut() {
+            match mbars.affinity {
+                Affinity::Anger => mbars.size = player.anger,
+                Affinity::Joy => mbars.size = player.joy,
+                Affinity::Fear => mbars.size = player.fear,
+                Affinity::Sadness => mbars.size = player.sadness,
+            }
+        }
+
+        if keyboard_input.just_pressed(KeyCode::W) || keyboard_input.just_pressed(KeyCode::A) || keyboard_input.just_pressed(KeyCode::S) || keyboard_input.just_pressed(KeyCode::D) {
+            println!("Starting mana values: {}, {}, {}, {}", player.anger, player.joy, player.fear, player.sadness);
+        }
+        if keyboard_input.just_released(KeyCode::W) || keyboard_input.just_released(KeyCode::A) || keyboard_input.just_released(KeyCode::S) || keyboard_input.just_released(KeyCode::D) {
+            println!("Stopping mana values: {}, {}, {}, {}", player.anger, player.joy, player.fear, player.sadness);
+        }
+
         let start_bounce = keyboard_input.just_pressed(KeyCode::Left) || keyboard_input.just_pressed(KeyCode::Right) || keyboard_input.just_pressed(KeyCode::Up) || keyboard_input.just_pressed(KeyCode::Down);
 
         if stop_bounce && player.bounce_speed > 0. {
@@ -115,6 +157,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_system(player_movement.system())
+        .add_system(update_manabars.system())
         .run();
 }
 
@@ -143,4 +186,66 @@ fn setup(
         fear: 0.,
         sadness: 0.,
     });
+
+    let barsize = Vec2::new(10., 10.);
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.9, 0.1, 0.1).into()),
+        sprite: Sprite{
+            size: barsize,
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .insert(ManaBar{
+        size: barsize.x,
+        pos: Vec2::new(10.,10.),
+        affinity: Affinity::Anger,
+    })
+    .insert(Affinity::Anger);
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.9, 0.9, 0.1).into()),
+        sprite: Sprite{
+            size: barsize,
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .insert(ManaBar{
+        size: barsize.x,
+        pos: Vec2::new(25.,10.),
+        affinity: Affinity::Joy,
+    })
+    .insert(Affinity::Joy);
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.1, 0.9, 0.1).into()),
+        sprite: Sprite{
+            size: barsize,
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .insert(ManaBar{
+        size: barsize.x,
+        pos: Vec2::new(40.,10.),
+        affinity: Affinity::Fear,
+    })
+    .insert(Affinity::Fear);
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.1, 0.1, 0.9).into()),
+        transform: Transform {
+            translation: Vec3::new(55.,10.,0.),
+            ..Default::default()
+        },
+        sprite: Sprite{
+            size: barsize,
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .insert(ManaBar{
+        size: barsize.x,
+        pos: Vec2::new(55.,10.),
+        affinity: Affinity::Sadness,
+    })
+    .insert(Affinity::Sadness);
 }
